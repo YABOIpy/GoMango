499491907171dd8c7993061612156b265228ab9c
YABOI <110062350+YABOIpy@users.noreply.github.com>
2023-04-13 21:27:33 +0200

    Update modules.go

--- transfer/modules.go
+++ transfer/modules.go
@@ -2,9 +2,31 @@ package transfer
 
 import (
 	"crypto/tls"
+	"encoding/json"
 	"github.com/hashicorp/hcl/v2/hclsimple"
 	"log"
+	"os"
 )
+
+func (Go *Mango) Errs(err error) {
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func (Go *Mango) Config(name string) Config {
+	var config Config
+	conf, err := os.Open(name)
+
+	defer conf.Close()
+	Go.Errs(err)
+
+	xp := json.NewDecoder(conf)
+	xp.Decode(&config)
+
+	return config
+
+}
 
 func (Go *Mango) TransportCfg(conf *tls.Config) {
 	Go.Cfg.Transport = conf
