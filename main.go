a50347237323a24ef0ebf4567f082e253bacacf6
YABOI <110062350+YABOIpy@users.noreply.github.com>
2023-04-13 21:24:04 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"GoMango/src"
+	"GoMango/transfer"
+	"fmt"
+)
+
+var (
+	c = src.X()
+	d = transfer.X()
+)
+
 func main() {
+	var choice int
+	fmt.Print(`
+		[1] Server [2] Client
+	`)
+	fmt.Scanln(&choice)
+	switch choice {
+	case 1:
+		d.Server_Listen()
+	case 2:
+		var msg string
+		fmt.Print("</>")
+		fmt.Scanln(&msg)
+		d.Dial(msg)
+	default:
+		main()
+	}
 
 }
