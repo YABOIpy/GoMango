269d1594f78c8682c20d16e4b9c2b6e82c858443
YABOI <110062350+YABOIpy@users.noreply.github.com>
2023-06-19 00:30:21 +0200

    Update main.go

--- main.go
+++ main.go
@@ -2,31 +2,9 @@ package main
 
 import (
 	t "GoMango/transfer/Server"
-
-	"fmt"
 )
 
-var ()
-
-// make a project tab to see all the available projects
-// write documentation for it
 func main() {
-	t.Run()
-	var choice int
-	fmt.Print(`
-		[1] Server [2] Client
-	`)
-	fmt.Scanln(&choice)
-	switch choice {
-	case 1:
-		//go d.ServerListen(mux.NewRouter())
-	case 2:
-		var msg string
-		fmt.Print("</>")
-		fmt.Scanln(&msg)
-		//d.Dial(msg)
-	default:
-		main()
-	}
-
+	go t.HostHTML("8080")
+	t.HostPHP("8888")
 }
